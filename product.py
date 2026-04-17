8dc5445b858876c8bfe4e4858751d675048a7c38
rewyou22-arch <rewyou22@gmail.com>
2026-04-17 17:21:29 +0300

    init project, edit product

---
+++ product.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self,name,count):
+        self.name = name
+        self.count = count
+
+    def show_info(self):
+        print(f"{self.naim} - имя продукта\n {self.count} - количество продукта")
